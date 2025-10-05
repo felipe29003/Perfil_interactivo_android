e4caf19ad72799f6465cbb752cbc89fa04377094
felipe29003 <afeligutierrez@poligran.edu.co>
2025-10-05 02:35:32 -0500

    Vista de videos ("es similar a la vista de fotos")

--- app/src/main/java/com/example/perfil_interactivo_android/ui/theme/VideoScreen.kt
+++ app/src/main/java/com/example/perfil_interactivo_android/ui/theme/VideoScreen.kt
@@ -1,7 +1,15 @@
 package com.example.perfil_interactivo_android.ui.theme
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
@@ -10,14 +18,47 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.example.perfil_interactivo_android.data.Lugares
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.perfil_interactivo_android.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.window.Dialog
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.draw.shadow
+import kotlin.math.absoluteValue
+import androidx.compose.ui.util.lerp
+import coil.compose.AsyncImage
+
 
 @Composable
 fun Video(navController: NavHostController) {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+
     Header(
         navController = navController,
         drawerState = drawerState,
@@ -27,11 +68,53 @@ fun Video(navController: NavHostController) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .background(Color(0xFF00325F))
                 .padding(paddingValues),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Pantalla de video", fontSize = 24.sp)
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(30.dp))
+                    .height(650.dp)
+                    .background(Color.White)
+                    .padding(30.dp)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = "Videos",
+                        fontFamily = roboto,
+                        fontWeight = FontWeight.Black,
+                        fontSize = 36.sp,
+                        modifier = Modifier
+                            .width(200.dp)
+                            .padding(top = 30.dp, end = 70.dp),
+                        textAlign = TextAlign.Center,
+                    )
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.icono_agregar),
+                    contentDescription = "Imagen clickeable",
+                    modifier = Modifier
+                        .padding(bottom = 50.dp)
+                        .align(Alignment.BottomEnd)
+                        .size(80.dp)
+                )
+            }
         }
     }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun VideoPreview() {
+    val navController = rememberNavController()
+    Video(navController = navController)
 }
