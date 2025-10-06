25a2ec6efe76b376fa4ff9e43fdad1d4b361bb66
felipe29003 <afeligutierrez@poligran.edu.co>
2025-10-05 23:15:50 -0500

    Implementacion de Android view para la vista "Web"

--- app/src/main/java/com/example/perfil_interactivo_android/ui/theme/WebScreen.kt
+++ app/src/main/java/com/example/perfil_interactivo_android/ui/theme/WebScreen.kt
@@ -1,5 +1,7 @@
 package com.example.perfil_interactivo_android.ui.theme
 
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -18,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,19 +29,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.runtime.getValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SearchBar
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.viewinterop.AndroidView
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Web(navController: NavHostController) {
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
-    var URLWeb by remember { mutableStateOf("") }
+    var URLWeb by remember { mutableStateOf("https://es.wikipedia.org/wiki/Wikipedia:Portada") }
     var estadobar by remember { mutableStateOf(false) }
 
     Header(
@@ -103,27 +104,20 @@ fun Web(navController: NavHostController) {
                     .weight(1f),
                 contentAlignment = Alignment.Center
             ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = "placeholder web",
-                        fontFamily = roboto,
-                        fontWeight = FontWeight.Medium,
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
+                AndroidView(factory ={
+                    WebView(it).apply {
+                        webViewClient = WebViewClient()
+                        settings.javaScriptEnabled = true
+                        settings.builtInZoomControls = true
+                        settings.displayZoomControls = false
+                        loadUrl(URLWeb)
+                    }
+                } )
 
             }
         }
     }
 }
-
-
-
-
 
 @Preview(showBackground = true)
 @Composable
